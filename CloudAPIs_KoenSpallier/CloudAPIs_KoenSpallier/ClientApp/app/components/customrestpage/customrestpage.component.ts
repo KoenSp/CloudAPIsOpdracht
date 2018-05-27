331b9fa3d25e1a4aa269ac5494823f5648357737
Unknown <spallierkoen@hotmail.com>
2018-05-27 17:39:18 +0200

    Added necessary pages
    Added necessary pages for the pokemon api and the custom rest api

---
+++ CloudAPIs_KoenSpallier/CloudAPIs_KoenSpallier/ClientApp/app/components/customrestpage/customrestpage.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'customrestpage',
+    templateUrl: './customrestpage.component.html'
+})
+export class CustomRestPageComponent {
+}
---
+++ CloudAPIs_KoenSpallier/CloudAPIs_KoenSpallier/ClientApp/app/components/pokemonpage/pokemonpage.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pokemonpage',
+    templateUrl: './pokemonpage.component.html'
+})
+export class PokemonPageComponent {
+}
