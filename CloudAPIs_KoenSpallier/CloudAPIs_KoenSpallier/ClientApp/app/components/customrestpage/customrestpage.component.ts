0c0f78e79445272243c06cd21d8cbed9fc25bc23
Unknown <spallierkoen@hotmail.com>
2018-06-02 15:56:52 +0200

    Poke Api implemantion finished

--- CloudAPIs_KoenSpallier/CloudAPIs_KoenSpallier/ClientApp/app/components/customrestpage/customrestpage.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'customrestpage',
-    templateUrl: './customrestpage.component.html'
-})
-export class CustomRestPageComponent {
-}
--- CloudAPIs_KoenSpallier/CloudAPIs_KoenSpallier/ClientApp/app/components/pokemonpage/pokemonpage.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'pokemonpage',
-    templateUrl: './pokemonpage.component.html'
-})
-export class PokemonPageComponent {
-}
